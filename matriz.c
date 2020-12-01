88ac262eeb9d982e35cae83b62a31c5e984a0cef
raulbau <raulb@111btec.com>
2020-12-01 11:59:05 -0600

    se agrego la multiplicacion

--- matriz.c
+++ matriz.c
@@ -35,14 +35,14 @@
 #include <time.h>
 #include "mpi.h"
 
-#define NUM_COLUMNAS1 10
-#define NUM_FILAS1 10
+#define NUM_COLUMNAS1 3
+#define NUM_FILAS1 3
 
-#define NUM_COLUMNAS2 10
-#define NUM_FILAS2 10
+#define NUM_COLUMNAS2 3
+#define NUM_FILAS2 3
 
 void llenar_matriz(float *arreglo, int tam_matriz);
-float multiplica_RenglonColumna(float renglon[], float columna[]);
+float multiplica_RenglonColumna(float fila[], float columna[]);
 void imprime_matriz_resultante(float matriz1[], float matriz2[], float matriz_resultante[]);
 
 int main(int argc, char *argv[])
@@ -67,35 +67,37 @@ int main(int argc, char *argv[])
     }
 
     //Declaración de variables para todos los procesos
-    int counts[num_proc], displs[num_proc]; // Arreglo de numero de datos y desplazamientos de cada proceso
+    int counts[num_procesos], displs[num_procesos]; // Arreglo de numero de datos y desplazamientos de cada proceso
     int tam_matriz1 = NUM_COLUMNAS1 * NUM_FILAS1;
     int tam_matriz2 = NUM_COLUMNAS2 * NUM_FILAS2;
 
     //Verificacion del numero de datos a enviar
-    num_datos = NUM_COLUMNAS1 / num_procesos;
-    num_datos_u = num_datos + NUM_COLUMNAS1 % (num_procesos);
+    num_datos = (NUM_COLUMNAS1 / num_procesos) * NUM_FILAS1;
+    num_datos_u = num_datos + ((NUM_COLUMNAS1 % num_procesos) * NUM_FILAS1);
+
     if (id_proceso == (num_procesos - 1))
         num_datos = num_datos_u;
 
     float *matriz_original, *matriz2_original, matriz_resultante[NUM_FILAS1 * NUM_COLUMNAS2];
-    float recibir_columna[NUM_COLUMNAS1], recibir_fila[NUM_FILAS2];
+    float recibir_columna[NUM_COLUMNAS1], recibir_fila[NUM_FILAS2], resultado;
 
     //Declarar las matrices solo para el proceso 0
     if (id_proceso == 0)
     {
-        matriz_original = (float *)malloc(tam_matriz1*sizeof(float));
-        matriz2_original = (float *)malloc(tam_matriz2*sizeof(float));
+        matriz_original = (float *)malloc(tam_matriz1 * sizeof(float));
+        matriz2_original = (float *)malloc(tam_matriz2 * sizeof(float));
 
         llenar_matriz(matriz_original, tam_matriz1);
         llenar_matriz(matriz2_original, tam_matriz2);
 
         // Inicializa los arreglos revcounts y displs
-        for(int i = 0; i < num_procesos; i++){
+        for (int i = 0; i < num_procesos; i++)
+        {
             counts[i] = num_datos;
             displs[i] = i * num_datos;
         }
-        
-        counts[num_procesos-1] = num_datos;
+
+        counts[num_procesos - 1] = num_datos;
     }
 
     //Pasar los datos a los procesos por MPI_Scatterv
@@ -103,11 +105,28 @@ int main(int argc, char *argv[])
     MPI_Scatterv(matriz2_original, counts, displs, MPI_FLOAT, &recibir_fila, num_datos, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     //Operar las columnas y renglones
+    resultado = multiplica_RenglonColumna(recibir_fila, recibir_columna);
 
     //Regresar el dato al proceso root
+    MPI_Gatherv(&resultado, 1, MPI_FLOAT, &matriz_resultante, counts, displs, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    if (id_proceso == 0)
+        imprime_matriz_resultante(matriz_original, matriz2_original, matriz_resultante);
 
     MPI_Finalize();
     return 0;
+}
+
+void copiaRenCol(char t, float *auxArr )
+{
+    switch (t)
+    {
+    case 'r':
+        break;
+
+    case 'c':
+        break;
+    }
 }
 
 void llenar_matriz(float *arreglo, int tam_matriz)
@@ -128,14 +147,14 @@ void llenar_matriz(float *arreglo, int tam_matriz)
     }
 };
 
-float multiplica_RenglonColumna(float renglon[], float columna[])
+float multiplica_RenglonColumna(float fila[], float columna[])
 {
     float res = 0.0;
     float arreglo_mult[NUM_COLUMNAS1];
 
     for (int i = 0; i < NUM_COLUMNAS1; i++)
     {
-        arreglo_mult[i] = renglon[i] * columna[i];
+        arreglo_mult[i] = fila[i] * columna[i];
     }
 
     for (int i = 0; i < NUM_COLUMNAS1; i++)
